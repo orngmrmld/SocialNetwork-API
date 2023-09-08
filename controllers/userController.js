a79c25ee4337cbd7784f5952a717e15850f23967
Vagabond <marcusjjreyes@gmail.com>
2023-09-08 13:33:40 -0700

    working update, delete and create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,42 +26,24 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
-  // create a new student
+  // create a new user
   createUser(req, res) {
-    Student.create(req.body)
-      .then((student) => res.json(student))
+    User.create(req.body)
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
 
   updateUser(req, res) {
-    Student.create(req.body)
-      .then((student) => res.json(student))
+    User.findOneAndUpdate({ _id: req.params.userId},{$set: req.body},{new: true})
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
 
   // Delete a student and remove them from the Thought
   deleteUser(req, res) {
-    Student.findOneAndRemove({ _id: req.params.studentId })
-      .then((student) =>
-        !student
-          ? res.status(404).json({ message: 'No such student exists' })
-          : Course.findOneAndUpdate(
-              { students: req.params.studentId },
-              { $pull: { students: req.params.studentId } },
-              { new: true }
-            )
-      )
-      .then((course) =>
-        !course
-          ? res.status(404).json({
-              message: 'Student deleted, but no courses found',
-            })
-          : res.json({ message: 'Student successfully deleted' })
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    User.findOneAndRemove({ _id: req.params.userId })
+    .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
   },
 
   // Add an assignment to a student
